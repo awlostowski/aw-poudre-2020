dba133249075b91c1a77a69283f7cd0ed4c7e5ad
Keith Jennings <keithjennings@Keiths-MacBook-Pro.local>
2020-06-16 09:32:27 -0600

    updating survey analysis code

--- econ/pwp_econ_survey_prelim.R
+++ econ/pwp_econ_survey_prelim.R
@@ -6,6 +6,7 @@
 
 # Load packages
 library(tidyverse)
+library(lubridate) # for parsing survey monkey datetimes
 library(cowplot); theme_set(theme_cowplot())
 
 # Import headers
@@ -16,5 +17,14 @@ headers <- unlist(strsplit(readLines("private_data/pwp_survey_headers.csv"),
 survey <- read.csv("private_data/pwp_survey_20200612.csv",
                    skip = 2, header = FALSE, col.names = headers)
 
+# Parse the date columns
+survey <- survey %>% 
+  mutate_at(vars(contains("date")), list(~ mdy_hms(., tz = "US/Mountain")))
 
 
+# Concatenate visit purposes
+# NA seems to correspond to "other"
+
+
+# Import Poudre gage data (for joining with flow ratings)
+flow <- 
