45c12e2b7266edb29ff4388a2bddb6a0a354a0e1
Keith Jennings <keithjennings@Keiths-MacBook-Pro.local>
2020-06-19 14:19:10 -0600

    updated plot code

--- flow_pref/survey-analysis/poudre_flow_pref_filter_plot.R
+++ flow_pref/survey-analysis/poudre_flow_pref_filter_plot.R
@@ -14,7 +14,11 @@ library(cowplot); theme_set(theme_cowplot())
 load(file = here("flow_pref", "survey-analysis", "respondend-attributes.Rdata"))
 load(file = here("flow_pref", "survey-analysis", "flow-pref-data.Rdata"))
 
-# First assign all users an experience code
+# Convert flow to numeric
+flowpref.dat <- flowpref.dat %>% 
+  mutate(flow = as.numeric(flow))
+
+# Assign all users an experience code
 respondent.attributes <- respondent.attributes %>% 
   mutate(skill = case_when(skill.novice == T ~ "novice",
                            skill.intermediate == T ~ "intermed",
@@ -44,22 +48,50 @@ segments <- unique(flowpref.dat$segment.name)
 # Loop through each segment
 # calculate flow preference
 # and plot the data
+for(i in 1:length(segments)){
+  segment_name = segments[i]
+  segment_name2 = str_replace_all(segment_name, pattern = " ", replacement = "")
+  results <- flowpref.dat %>%
+    
+    # Remove NAs and select a specific segment
+    filter(!is.na(preference.code) & 
+             segment.name == segment_name &
+             respondent.id %in% respondent.id.valid) %>%
+    
+    # calculate the average pref score and PCI2 statistic of each flow bin
+    group_by(flow) %>%
+    summarize(pref.average = mean(preference.code),   # average preference
+              d = sum(abs(dist(preference.code))),    # sum of the score distance vector
+              m = sum(abs(dist(rep(c(-3,3),n())))),   # maximum possible sum of distance vector
+              pci2 = d/m,                             # PCI2 = d/m
+              n_obs = length(flow))                   # count # of observations
+  
+  
+  # Plot
+  flow_pref_plot <- 
+    ggplot() +
+    geom_jitter(data = filter(flowpref.dat, segment.name == segment_name), 
+                aes(x = as.numeric(flow), 
+                    y = preference.code), 
+                size = 1, alpha = 0.3) +
+    geom_point(data = filter(results, n_obs > 2), 
+               aes(x = flow, y = pref.average, size = pci2), 
+               color = 'blue') +
+    labs(x = "Flow (cfs)",
+         y = "Preference Score",
+         title = paste0(segment_name, " Flow Preference Curve")) +
+    geom_hline(yintercept = 0)
+  
+  # Export plot
+  save_plot(filename = paste0("plots/flow_pref/flow_pref_",
+                              segment_name,
+                              ".png"),
+            plot = flow_pref_plot,
+            base_width = 7)
+  
+}
 
-segment_name = "Filter Plant"
-results <- flowpref.dat %>%
-  
-  # Remove NAs and select a specific segment
-  filter(is.na(preference.code) == 0 & segment.name == segment_name) %>%
-  
-  # calculate the average pref score and PCI2 statistic of each flow bin
-  group_by(as.numeric(flow)) %>%
-  summarize(pref.average = mean(preference.code),   # average preference
-            d = sum(abs(dist(preference.code))),    # sum of the score distance vector
-            m = sum(abs(dist(rep(c(-3,3),n())))),   # maximum possible sum of distance vector
-            pci2 = d/m) %>%                         # PCI2 = d/m
-  
-  # rename a variable
-  rename(flow = `as.numeric(flow)`)
+
 
 
 ggplot() +
