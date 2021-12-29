14f69a053336cc921d74058fcbbcaf04141ad5dd
anguswg-ucsb <anguswatters@gmail.com>
2021-12-29 13:50:14 -0800

    rating_utils file for flow data functions, added start of mass_balance_model script. Poudre River flow data ready to pull using rating_utils functions

--- rating_curves/rock_stage_webscrape.R
+++ rating_curves/rock_stage_webscrape.R
@@ -31,130 +31,135 @@ library(stringr)
 library(stringi)
 library(logger)
 
-##------------------------------------------------------------------------------
-## Function definitions
-
+# ------------------------------------------------------------------------------
+# Function definitions
 scrape_page <- function(n) {
-  # 
-  # Scrape stage observations at Pine View, Hewlett, and Rustic from RockReport
-  # blog posts.
-  #
-  # Args:
-  #   n (int): Rock Report page number to scrape
-  #
-  # Returns:
-  #   DataFrame of stage observations and metadata
+     
+    # Scrape stage observations at Pine View, Hewlett, and Rustic from RockReport
+    # blog posts.
     
-  logger::log_info("Scraping page {n}")
-
-  site <- paste0(
-    "http://www.poudrerockreport.com/category/flows/page/",
-    as.character(n)
-  )
-  
-  # identify articles (blog posts) on page n
-  articles <- rvest::read_html(site) %>%
-    rvest::html_elements("article")
-  
-  # extract post titles
-  titles <- rvest::html_attrs(articles) %>%
-    lapply(`[`, "aria-label") %>%
-    unlist(use.names = FALSE)
-  
-  # extract post dates
-  dates <- rvest::html_elements(articles, "time") %>%
-    rvest::html_text2()
-  
-  # extract post categories
-  categories <- rvest::html_elements(articles, ".entry-categories") %>%
-    rvest::html_text2() %>%
-    stringr::str_remove_all("Filed Under: ")
-  
-  # blog entry content text
-  entry_content = rvest::read_html(site) %>%
-    html_nodes(".entry-content") %>%
-    html_text()
-  
-  #
-  # ---- HEWLETT ----
-  #
-  
-  # locates the string "Hewlett" and select 100 characters just after it. 
-  J = str_locate(entry_content, pattern = "Hewlett")
-  hewlett_strings = str_sub(entry_content, J[, 1], J[, 2]+100) 
-  
-  # break "Hewlett"-containing strings into chunks separated by spaces
-  comps <- str_split(hewlett_strings, " ", simplify = T)
-  
-  # extract stage value from each post, loop over rows
-  hewlett_stage = c()
-  for (i in 1:nrow(comps)) {
-    # find "Feet"
-    I = which(str_detect(comps[i, ], "Feet|ft.|Ft."))
+    # Args:
+    #   n (int): Rock Report page number to scrape
     
-    # select the string following and convert to numeric
-    hewlett_stage[i] = as.numeric(comps[i, min(I)-1])
-  }
-  
-  #
-  # ---- RUSTIC ----
-  #
-  
-  # locates the string "Rustic" and select 100 characters just after it. 
-  J = str_locate(entry_content, pattern = "Rustic")
-  rustic_strings = str_sub(entry_content, J[,1], J[,2]+100) 
-  
-  # break "Rustic"-containing strings into chunks separated by spaces
-  comps <- str_split(rustic_strings, " ", simplify = T)
-  
-  # extract stage value from each post, loop over rows
-  rustic_stage = c()
-  for (i in 1:nrow(comps)) {
+    # Returns:
+    #   DataFrame of stage observations and metadata
     
-    # find "Feet"
-    I = which(str_detect(comps[i, ], "Feet|ft.|Ft."))
+    # scrape a page from  Poudre Rock Report
+    logger::log_info("Scraping page {n}")
     
-    # select the string following and convert to numeric
-    rustic_stage[i] = as.numeric(comps[i, min(I)-1])
+    site <- paste0(
+      "http://www.poudrerockreport.com/category/flows/page/",
+      as.character(n)
+      # "http://www.poudrerockreport.com/category/flows/page/", as.character(page[8])
+    )
     
-  }
-  
-  # remove special characters EXCEPT period
-  title_content <- stringi::stri_replace_all_fixed(
-                      titles, 
-                      c("{", "}", "~", "[", "]", "(", ")","!", "@", "<", ">"),
-                      c("", "", "", "", "","", "","", "", "", ""),
-                      vectorize_all = FALSE
-                      )
-  
-  pine_stage <- str_extract(title_content, "\\d+\\.*\\d*") %>% 
-    as.numeric()
-  
-  # convert post date to Date class
-  date = as.Date(dates, format = "%B %d, %Y")
-  
-  # pack scraped data into a dataframe
-  data.frame(
+    articles <- rvest::read_html(site) %>%
+      rvest::html_elements("article")
+    
+    # extract titles
+    titles <- rvest::html_attrs(articles) %>%
+      lapply(`[`, "aria-label") %>%
+      unlist(use.names = FALSE)
+    
+    # extract dates
+    dates <- rvest::html_elements(articles, "time") %>%
+      rvest::html_text2()
+    
+    categories <- rvest::html_elements(articles, ".entry-categories") %>%
+      rvest::html_text2() %>%
+      stringr::str_remove_all("Filed Under: ")
+    
+    # blog entry content text contains stage and flow observations at several sites
+    entry_content = rvest::read_html(site) %>%
+      html_nodes(".entry-content") %>%
+      html_text()
+    
+    # ---- HEWLETT ----
+    # locates the string "Hewlett" and select a bunch of characters just after it. 
+    J = str_locate(entry_content, pattern = "Hewlett") # locate
+    hewlett_strings = str_sub(entry_content, J[,1], J[,2]+100) # select trailing 100 characters
+    
+    # break "Hewlett"-containing strings into chunks separated by spaces
+    comps <- str_split(hewlett_strings, " ", simplify = T)
+    
+    # extract stage value from each post, loop over rows
+    hewlett_stage = c()
+    for (i in 1:nrow(comps)) {
+      # find "Feet"
+      I = which(str_detect(comps[i,], "Feet|ft.|Ft."))
+      
+      # select the string following and convert to numeric
+      hewlett_stage[i] = as.numeric(comps[i,min(I)-1])
+    }
+    
+    # ---- RUSTIC ----
+    # locates the string "Rustic" and select a bunch of characters just after it. 
+    J = str_locate(entry_content, pattern = "Rustic") # locate
+    rustic_strings = str_sub(entry_content, J[,1], J[,2]+100) # select trailing 100 characters
+    
+    # break "Rustic"-containing strings into chunks separated by spaces
+    comps <- str_split(rustic_strings, " ", simplify = T)
+    
+    # extract stage value from each post, loop over rows
+    rustic_stage = c()
+    for (i in 1:nrow(comps)) {
+      
+      # find "Feet"
+      I = which(str_detect(comps[i,], "Feet|ft.|Ft."))
+      
+      # select the string following and convert to numeric
+      rustic_stage[i] = as.numeric(comps[i,min(I)-1])
+      
+    }
+    # remove special characters EXCEPT period
+    title_content <- stringi::stri_replace_all_fixed(
+      titles, 
+      c("{", "}", "~", "[", "]", "(", ")","!", "@", "<", ">"),
+      c("", "", "", "", "","", "","", "", "", ""),
+      vectorize_all = FALSE
+    )
+    
+    pine_stage <- str_extract(title_content, "\\d+\\.*\\d*") %>% 
+      as.numeric()
+    
+    # convert post date to Date class
+    date   <- lubridate::mdy(dates)
+    # date = as.Date(dates, format = "%B %d %Y")
+    
+    data.frame(
       title        = titles,
       date         = date,
-      Pineview     = pine_stage,
-      Hewlett      = hewlett_stage,
-      Rustic       = rustic_stage,
-      page         = n,   
+      pineview     = pine_stage,
+      hewlett      = hewlett_stage,
+      rustic       = rustic_stage,
+      page         = n,   # page         = 187,
       categories   = categories
-      ) %>% 
-  mutate( 
-      # extract 4 digit military time from title and convert to hour, minutes
-      time = format( 
-        as.POSIXct(
-          sprintf("%04s", stri_extract_last(title, regex = "\\d{4}")),
-          format  = "%H%M",
-          origin  = "1970-01-01",
-          tz      = "UTC"
+    ) %>% 
+      mutate( 
+        time = format( # extract 4 digit military time from title and convert to hour, minutes
+          as.POSIXct(
+            sprintf("%04s", stri_extract_last(title, regex = "\\d{4}")),
+            format  = "%H%M",
+            origin  = "1970-01-01",
+            tz      = "UTC"
           ), "%H:%M"
         )
-      )
-}
+      ) %>% 
+      mutate(
+        time = case_when(
+          str_detect(title, regex('noon', ignore_case = T)) == TRUE ~ "12:00",
+          str_detect(title, regex('noon', ignore_case = T)) == FALSE ~ time
+        )
+      ) %>%
+      mutate(
+        time = format(
+          round(
+            strptime(paste("2001-01-01", time), format="%Y-%m-%d %H:%M"), 
+            units="hours"), 
+          format="%H:%M")) %>% 
+      mutate(datetime = as.POSIXct(paste(as.character(date), time),  format = "%Y-%m-%d %H:%M")) %>% 
+      dplyr::relocate(title, categories, page, date, time, datetime, pineview, hewlett, rustic)
+  }
 
 ##------------------------------------------------------------------------------
 ## Executed statements
