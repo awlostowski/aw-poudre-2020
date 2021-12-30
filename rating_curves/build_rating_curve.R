7d330000db7a94bc424ca0d10668ceaa9bd0c9ca
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-12-30 16:55:01 -0500

    revise rating curve workflow to leverage flow data functions

--- rating_curves/build_rating_curve.R
+++ rating_curves/build_rating_curve.R
@@ -26,6 +26,9 @@
 remove(list = ls())  # clear all workspace variables
 cat("\014")          # clear command line
 
+# Suppress summarise info
+options(dplyr.summarise.inform = FALSE)
+
 ## load packages
 library(here)
 library(tidyverse)
@@ -34,6 +37,7 @@ library(nlstools)
 library(rootSolve) 
 library(stringr)
 library(logger)
+source(here::here('rating_curves', 'get_flow_utils.R'))
 
 ##------------------------------------------------------------------------------
 ## Function definitions
@@ -54,14 +58,8 @@ CheckLoad <- function(path, filename) {
     loaded.data <- readRDS(paste0(path, "/", filename))
     
   } else {
-    logger::log_error("{filename} not found!")
-    logger::log_error(
-      "Run rock_stage_webscrape.R to build Pineview stage record."
-    )
-    logger::log_error(
-      "Run get_FoCo_OpenData_flow.R to build tidy Poudre Park flow record."
-    )
-    stop()
+    logger::log_warn("{filename} not found!")
+    loaded.data = F
   }
   
   return(loaded.data)
@@ -73,63 +71,36 @@ CheckLoad <- function(path, filename) {
 # =======================================
 # Read-in stage and flow data
 
+#*****
+# TODO(awlostowski): Look for data, if it doesn't exist make it/get it
+#*****
+
 # Read in Rock Report stage observations at Pineview
 rockreport.path     <- here::here("data","rock_report")
 rockreport.filename <- "stage_poudre_rock_report.RDS"
-rock.stage          <- CheckLoad(rockreport.path, rockreport.filename)
+stage          <- CheckLoad(rockreport.path, rockreport.filename)
+if (stage == FALSE) {
+  logger::error('Please run rock_stage_webscrape.R to get stage data')
+  stop()
+}
   
 # Read in gage observations at Poudre Park
 gagedata.path     <- here::here("data","gauge")
 gagedata.filename <- "poudre_park_flow.RDS"
-gage.flow <- CheckLoad(gagedata.path, gagedata.filename)
+flow <- CheckLoad(gagedata.path, gagedata.filename)
+if (flow == FALSE) {
+  logger::log_info(
+    'Attempting to get Poudre Park flow data from FoCo OpenData...'
+    )
+  flow <- getOpenDataFlow("Poudre Park", save.data = T)
+}
 
 # =======================================
-# Align datetime conventions of each dataset and join
-
-# specify stage observation date and time
-stage <- rock.stage %>%
-  mutate(
-    time = case_when(
-      str_detect(title, regex('noon', ignore_case = T)) == TRUE ~ "12:00",
-      str_detect(title, regex('noon', ignore_case = T)) == FALSE ~ time
-    )
-  ) %>%
-  mutate(
-    time = format(
-      round(
-        strptime(paste("2001-01-01", time), format="%Y-%m-%d %H:%M"), 
-            units="hours"), 
-      format="%H:%M")) %>% 
-  filter(is.na(time) == F) %>% 
-  mutate(date = as.POSIXct(paste(as.character(date), time),
-                           format = "%Y-%m-%d %H:%M"))
-
-# round flow time to nearest hour, make date variable full datetime, 
-# and average flow observations within the same hour
-flow <- gage.flow %>% 
-  mutate(time = format(
-            round(
-              strptime(
-                paste("2001-01-01", gage.flow$time),
-                format="%Y-%m-%d %H:%M"
-                ), 
-              units="hours"
-              ), 
-            format="%H:%M"),
-         date = as.POSIXct(
-           paste(as.character(date), time),
-           format = "%Y-%m-%d %H:%M"
-           )
-         ) %>% 
-  group_by(date) %>% 
-  summarize(
-    flow_cfs = mean(flow_cfs, na.rm = T)
-  )
-
 # Join observed gage flow and RockReport stage observations 
-rating.data <- inner_join(stage, flow, by = "date") %>% 
-  rename(stage = Pineview, flow = flow_cfs) %>%
-  select(date, stage, flow)
+
+rating.data <- inner_join(stage, flow, by = "datetime") %>% 
+  rename(stage = pineview, flow = flow) %>%
+  select(datetime, stage, flow)
 
 logger::log_info(
   "there are {nrow(rating.data)} flow/stage observations in the rating data"
@@ -182,40 +153,40 @@ path <- here::here("rating_curves")
 logger::log_info("saving a plot of Pineview rating curve at {path}")
 ggsave(paste0(path, "/pineview_rating.png"))
 
-# =======================================
-# Evaluate relationship between flows at Poudre Park and Rustic
-
-rustic.filename <- "rustic_flow.RDS"
-rustic.flow <- CheckLoad(gagedata.path, rustic.filename)
-
-# take hourly average of flow data at Rustic
-flow.rustic.dly <- rustic.flow %>% 
-  group_by(date) %>% 
-  summarize(
-    flow_cfs = mean(flow_cfs, na.rm = T)
-  )
-
-flow.poudre.dly <- gage.flow %>%
-  group_by(date) %>%
-  summarize(
-    flow_cfs = mean(flow_cfs, na.rm = T)
-  )
-
-# join Poudre Park and Rustic flows
-site.compare <- inner_join(flow.rustic.dly, flow.poudre.dly, by = "date") %>% 
-  rename(Rustic = flow_cfs.x, Poudre.Park = flow_cfs.y) %>%
-  select(date, Poudre.Park, Rustic) %>%
-  mutate(month = month(date))
-
-compare.plot <- ggplot() +
-  geom_point(data = site.compare, aes(x = Poudre.Park, y = Rustic, color = date)) +
-  labs( 
-    y = "Flow at Rustic (cfs)",
-    x= "Flow at Poudre Park (cfs)"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 14),
-    axis.title  = element_text(size = 14)
-  )
-print(compare.plot)
+# # =======================================
+# # Evaluate relationship between flows at Poudre Park and Rustic
+# 
+# rustic.filename <- "rustic_flow.RDS"
+# rustic.flow <- CheckLoad(gagedata.path, rustic.filename)
+# 
+# # take hourly average of flow data at Rustic
+# flow.rustic.dly <- rustic.flow %>% 
+#   group_by(date) %>% 
+#   summarize(
+#     flow_cfs = mean(flow_cfs, na.rm = T)
+#   )
+# 
+# flow.poudre.dly <- gage.flow %>%
+#   group_by(date) %>%
+#   summarize(
+#     flow_cfs = mean(flow_cfs, na.rm = T)
+#   )
+# 
+# # join Poudre Park and Rustic flows
+# site.compare <- inner_join(flow.rustic.dly, flow.poudre.dly, by = "date") %>% 
+#   rename(Rustic = flow_cfs.x, Poudre.Park = flow_cfs.y) %>%
+#   select(date, Poudre.Park, Rustic) %>%
+#   mutate(month = month(date))
+# 
+# compare.plot <- ggplot() +
+#   geom_point(data = site.compare, aes(x = Poudre.Park, y = Rustic, color = date)) +
+#   labs( 
+#     y = "Flow at Rustic (cfs)",
+#     x= "Flow at Poudre Park (cfs)"
+#   ) +
+#   theme_bw() +
+#   theme(
+#     axis.text = element_text(size = 14),
+#     axis.title  = element_text(size = 14)
+#   )
+# print(compare.plot)
